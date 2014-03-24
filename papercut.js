73db059aa24dfd7b08bb6df3a817c59944bfc8a1
Lloyd Dewolf <jollylloyd@gmail.com>
2014-03-24 09:46:27 -0700

    First commit

---
+++ papercut.js
@@ -0,0 +1,11 @@
+if (Meteor.isClient) {
+  Template.hello.greeting = function () {
+    return "Free yourself of the paper you never read, you never wanted.";
+  };
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
