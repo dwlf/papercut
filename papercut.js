350fd1a1352cde8fbd765b35a8b641d8d620cedd
Lloyd Dewolf <jollylloyd@gmail.com>
2014-03-24 14:50:32 -0700

    Content: front page using some bootstrap structurej Based on - http://www.sitepoint.com/twitter-bootstrap-tutorial-handling-complex-designs/ - http://getbootstrap.com/components/#navbar-fixed-bottom

--- papercut.js
+++ papercut.js
@@ -1,11 +1,11 @@
 if (Meteor.isClient) {
   Template.hello.greeting = function () {
-    return "Free yourself of the paper you never read, you never wanted.";
-  };
+    return "Free yourself of the paper you never read, you never wanted."
+  }
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
-  });
+  })
 }
