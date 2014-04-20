13ffde64d1aec06097b94a7cef99f54fa4d9253d
Lloyd Dewolf <jollylloyd@gmail.com>
2014-04-20 15:40:28 -0700

    Not found: general & newspaper/

--- client/helpers/newspaper_helpers.js
+++ client/helpers/newspaper_helpers.js
@@ -1,45 +1,30 @@
-UI.registerHelper('outputOrEditMe', function( key, value ) {
-  if ( !value ) {
-    value = 'Add ' + key + '.';
-  } else {
-    if ( key === 'url') {
-      var a = document.createElement('a');
-      a.href = value;
-      value = '<a href="' + value + '">' + a.hostname + a.pathname + '</a>';
+Template.newspaperPage.helpers({
+  newspaperExists: function() {
+    var exists = false;
+    console.log( JSON.stringify(this) );
+    if ( this.findOne() ) {
+      exists = true;
     }
+    return exists;
   }
-  return value;
 });
 
-Template.newspaperPage.echoOrEditMe = function( key, value ) {
-  if (!value) {
-    value = 'Add ' + key + '.';
-  } else {
-    if ( key === 'url') {
-      var a = document.createElement('a');
-      a.href = value;
-      value = '<a href="' + value + '">' + a.hostname + a.pathname + '</a>';
-    }
-  }
-  return value;
-}
-
-Template.newspaperPage.htmlFullNewspaper = function( ) {
+Template.newspaperPage.htmlFullNewspaper = function() {
   var html = '';
   for (key in this) {
-    if ( key === '_id' ) {
+    if (key === '_id') {
       continue;
     }
     html = html + '<div>';
-    if ( this[key] ) {
-      switch( key ) {
+    if (this[key]) {
+      switch (key) {
         case 'url':
           var a = document.createElement('a');
           a.href = this[key];
           html = html + '<a href="' + this[key] + '">' + a.hostname + a.pathname + '</a>';
           break;
         case 'address':
-          html = html + JSON.stringify( this[key] );
+          html = html + JSON.stringify(this[key]);
           break;
         default:
           html = html + key + ': ' + this[key];
