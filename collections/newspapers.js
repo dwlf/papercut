9599ddf4a657dfe9226a41e232b9bbb08a044cfb
Lloyd Dewolf <jollylloyd@gmail.com>
2014-04-20 08:13:51 -0700

    Add Newspaper asks you to sign in

--- collections/newspapers.js
+++ collections/newspapers.js
@@ -1,1 +1,7 @@
 Newspapers = new Meteor.Collection('newspapers');
+
+Newspapers.allow({
+  insert: function(userId, doc) {
+    // only allow posting if you are logged in
+    return !! userId; }
+});
